b7e8260179b6bcef5f3813eff5ea02d18accb819
ugm616 <67227131+ugm616@users.noreply.github.com>
2024-12-23 13:28:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -111,18 +111,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
-        modal.style.display = "block";
+        modal.classList.add("show");
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-        modal.style.display = "none";
+        modal.classList.remove("show");
+        setTimeout(function() {
+            modal.style.display = "none";
+        }, 500); // Wait for the transition to finish
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.style.display = "none";
+            modal.classList.remove("show");
+            setTimeout(function() {
+                modal.style.display = "none";
+            }, 500); // Wait for the transition to finish
         }
     }
 });
